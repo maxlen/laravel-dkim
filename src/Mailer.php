8794beb0fdce5d91955766eb1657b57c49b78a8e
ML <maxlenash@gmail.com>
2020-01-06 21:45:06 +0200

    start

---
+++ src/Mailer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace maxlen\LaravelDkim;
+
+use Swift_Message;
+
+class Mailer extends \Illuminate\Mail\Mailer
+{
+    protected function createMessage()
+    {
+        $message = new Message(new Swift_Message);
+
+        // If a global from address has been specified we will set it on every message
+        // instances so the developer does not have to repeat themselves every time
+        // they create a new message. We will just go ahead and push the address.
+        if (! empty($this->from['address'])) {
+            $message->from($this->from['address'], $this->from['name']);
+        }
+
+        if (in_array(strtolower(config('mail.driver')), ['smtp', 'sendmail', 'log', 'mail'])) {
+            if (config('mail.dkim_private_key') && file_exists(config('mail.dkim_private_key'))) {
+                if (config('mail.dkim_selector') && config('mail.dkim_domain')) {
+                    $message->attachDkim(config('mail.dkim_selector'), config('mail.dkim_domain'), file_get_contents(config('mail.dkim_private_key')), config('mail.dkim_passphrase'));
+                }
+            }
+        }
+
+        return $message;
+    }
+
+}
---
+++ src/Message.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace maxlen\LaravelDkim;
+
+use Swift_Signers_DKIMSigner;
+use Swift_SwiftException;
+
+class Message extends \Illuminate\Mail\Message
+{
+    /**
+     * @param $selector
+     * @param $domain
+     * @param $privateKey
+     * @param $passphrase
+     *
+     * @return $this
+     * @throws Swift_SwiftException
+     */
+    public function attachDkim($selector, $domain, $privateKey, $passphrase = '')
+    {
+        $signer = new Swift_Signers_DKIMSigner($privateKey, $domain, $selector, $passphrase);
+        $signer->setHashAlgorithm(config('mail.dkim_algo', 'rsa-sha256'));
+        if (config('mail.dkim_identity')) {
+            $signer->setSignerIdentity(config('mail.dkim_identity'));
+        }
+        $this->swift->attachSigner($signer);
+
+        return $this;
+    }
+}
